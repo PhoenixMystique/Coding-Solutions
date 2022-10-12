045b29c2dc3a1f999aa0fc1a5f9a1694a8356d96
Aditya Tanwar <adityatanwarofficial@gmail.com>
2022-10-12 17:28:53 +0530

    day2

--- day2/reverseInGrops.java
+++ day2/reverseInGrops.java
@@ -1,25 +1,30 @@
 package day2;
 
+import java.util.ArrayList;
+
 public class reverseInGrops {
     public static void main(String[] args) {
-  int[] data ={1,2,3,4,5};
-    reverseInGroups(data,5,3);
+        ArrayList<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+    reverseInGroups(list,5,3);
     }
-    static void reverseInGroups(int arr[], int n, int k)
+    static void reverseInGroups(ArrayList arr, int n, int k)
     {
         for (int i = 0; i < n; i += k) {
             int part1 = i;
          int part2 = Math.min(i + k - 1, n - 1);
          while (part1 <part2) {
-               arr[part1]+=arr[part2];
-               arr[part2]=arr[part1]-arr[part2];
-                arr[part1]=arr[part1]- arr[part2];
+               int temp = (int) arr.get(part1);
+                arr.set(part1,arr.get(part2));
+                arr.set(part2,temp);
                 part1+=1;
                 part2-=1;
             }
         }
-        for(int i:arr){
-            System.out.println(i);
-        }
+        System.out.println(arr.toString());
     }
 }
